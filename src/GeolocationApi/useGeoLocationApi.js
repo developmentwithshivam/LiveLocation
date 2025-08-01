32bff9eb97fce0f121c945fe422c26dfa92f7922
developmentwithshivam <developmentwithshivam@gmail.com>
2025-08-01 23:20:57 +0530

    fix mobile bug

--- src/GeolocationApi/useGeoLocationApi.js
+++ src/GeolocationApi/useGeoLocationApi.js
@@ -24,7 +24,7 @@ function useGeoLocationApi() {
         {
           // enableHighAccuracy: true,
           maximumAge: 0,
-          timeout: 10000,
+          timeout: 20000,
         }
       );
     } else {
